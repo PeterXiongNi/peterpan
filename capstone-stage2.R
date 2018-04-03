80db642bcede91ed2b30a2eab03f39c0c44865df
Peter Xiong Ni <xiongni.peter@gmail.com>
2018-04-03 22:45:46 +0200

    first attempt on build model on chunk and merge it

--- capstone-stage2.R
+++ capstone-stage2.R
@@ -1,5 +1,5 @@
 # Setup working directory
-# setwd("~/Documents/DataScience/Coursera_data_science_specialization/capstone_project")
+# setwd("~/Development/capstone/")
 library(quanteda)
 library(data.table)
 library(R.utils)
@@ -33,13 +33,13 @@ data$text <- read_rds("rawData/train.rds")
 data$chunkSize <- 50000
 data$length <- length(data$text)
 data$index <- index(data)
-data$tokens <- pre_process(data, 4)
+data$model <- build_ngram_model(data, 4)
 if (!dir.exists("cleanData")) dir.create("cleanData")
 saveRDS(data, "cleanData/tokenized_train.rds")
 data <- read_rds("cleanData/tokenized_train.rds")
 
 index <- function(data) {
-  # chunkSize <- data$chunkSize
+  chunkSize <- data$chunkSize
   length <- data$length
   rows <- floor(length / chunkSize)
   idx_minus_last_row <- matrix(1:(chunkSize * rows), nrow = rows, ncol = chunkSize, byrow = TRUE)
@@ -51,29 +51,29 @@ index <- function(data) {
   return(rbind(idx_minus_last_row, idx_last_row))
 }
 
-pre_process <- function(data, n) {
+build_ngram_model <- function(data, n) {
   text <- data$text
   idx <- data$index
   chunkSize <- data$chunkSize
   rows <- floor(data$length / chunkSize)
-  ngram_tokens <- c()
+  model <- initial_model(n)
   for (i in 1:rows) {
     t <- Sys.time()
     corpus <- corpus(text[idx[i,]])
-    ngram_tokens <- c(ngram_tokens, 
-                      tokens(corpus, "word",
-                             remove_numbers = TRUE,
-                             remove_punct = TRUE,
-                             remove_symbols = TRUE,
-                             remove_hyphens = TRUE,
-                             remove_url = TRUE,
-                             ngrams = n) %>%
-                        tokens_tolower() %>%
-                        tokens_select(stopwords("en"), selection = "remove"))
+    ngram_tokens <- tokens(corpus, "word",
+                           remove_numbers = TRUE,
+                           remove_punct = TRUE,
+                           remove_symbols = TRUE,
+                           remove_hyphens = TRUE,
+                           remove_url = TRUE,
+                           ngrams = n) %>%
+                      tokens_tolower() %>%
+                      tokens_select(stopwords("en"), selection = "remove")
     printf("chunk %d tokenized in %.3f s\n", i, Sys.time() - t)
-    t <- system.time(dt <- build_ngram_model(ngram_tokens, n))
-    printf("model built with chunk %d in %.3f s\n\n", i, t[3])
-    
+    t <- system.time(chunk_model <- chunk_build_model(ngram_tokens, n))
+    printf("chunk_model built with chunk %d in %.3f s\n", i, t[3])
+    t <- system.time(model <- merge_chunks(model, chunk_model))
+    printf("No.%d chunk_model merged with model in %.3f s\n\n\n", i, t[3])
   }
   t <- Sys.time()
   last_row_idx <- idx[rows + 1, ]
@@ -90,13 +90,32 @@ pre_process <- function(data, n) {
                           ngrams = n) %>%
                      tokens_tolower() %>%
                      tokens_select(stopwords("en"), selection = "remove"))
-  ...
+  printf("chunk %d tokenized in %.3f s\n", rows + 1, Sys.time() - t)
+  t <- system.time(chunk_model <- chunk_build_model(ngram_tokens, n))
+  printf("chunk_model built with chunk %d in %.3f s\n", rows + 1, t[3])
+  t <- system.time(model <- merge_chunks(model, chunk_model))
+  printf("%d chunk_model merged with model in %.3f s\n\n\n", rows + 1, t[3])
+  return(summarize_model(model, n))
 }
 
-build_ngram_model <- function(ngram_tokens, n) {
+initial_model <- function(n) {
+  dt <- data.table(ngram = character(), n = integer(), w1 = character())
+  if (n == 2) {
+    dt <- dt[, w2 := character()]
+  } else if (n == 3) {
+    dt <- dt[, w2 := character()]
+    dt <- dt[, w3 := character()]
+  } else if (n == 4) {
+    dt <- dt[, w2 := character()]
+    dt <- dt[, w3 := character()]
+    dt <- dt[, w4 := character()]
+  }
+}
+
+chunk_build_model <- function(ngram_tokens, n) {
   dfm <- dfm(ngram_tokens)
   feature_fre <- colSums(dfm)
-  dt <- data.table(ngram = names(featureFre), n = featureFre)
+  dt <- data.table(ngram = names(feature_fre), n = feature_fre)
   tmp <- str_split(dt$ngram, pattern = "_")
   dt[, w1:= sapply(tmp, "[", 1)]
   if (n == 2) {
@@ -125,7 +144,7 @@ summarize_model <- function(dt, n) {
   return(dt)
 }
 
-mergeChunks <- function(a, b) {
+merge_chunks <- function(a, b) {
   setkey(a, ngram);setkey(b, ngram)
   a$n[a$ngram %in% b$ngram] <-  a$n[a$ngram %in% b$ngram] + b$n[b$ngram %in% a$ngram]
   a <- rbindlist(list(a, b[!ngram %in% a$ngram]))
